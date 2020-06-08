0b853746aee248c6d653ea01265d090839b124a9
Yash <yaswanth.iddibani@gmail.com>
2020-06-08 23:05:08 +0100

    Custom HTTP Lib

--- 05_customHTTPLibrary.js
+++ 05_customHTTPLibrary.js
@@ -1,4 +1,57 @@
 const http = new EasyHttp();
+const createData = {
+  title: "foo",
+  body: "bar",
+  userId: 1,
+};
+
+const updateDate = {
+  title: "foo",
+  body: "bar",
+  userId: 1,
+};
 
 // Get Posts
-http.get("https://jsonplaceholder.typicode.com/posts");
+// http.get("https://jsonplaceholder.typicode.com/posts", function (
+//   error,
+//   response
+// ) {
+//   if (error) console.log(error);
+//   else console.log(response);
+// });
+
+// // Get Single Post
+// http.get("https://jsonplaceholder.typicode.com/posts/1", function (
+//   error,
+//   response
+// ) {
+//   if (error) console.log(error);
+//   else console.log(response);
+// });
+
+// // Create Post
+// http.post("https://jsonplaceholder.typicode.com/posts", createData, function (
+//   error,
+//   response
+// ) {
+//   if (error) console.log(error);
+//   else console.log(response);
+// });
+
+// // Update Post
+// http.put("https://jsonplaceholder.typicode.com/posts/1", updateDate, function (
+//   error,
+//   response
+// ) {
+//   if (error) console.log(error);
+//   else console.log(response);
+// });
+
+// Delete Post
+http.delete("https://jsonplaceholder.typicode.com/posts/1", function (
+  error,
+  response
+) {
+  if (error) console.log(error);
+  else console.log(response);
+});
--- easyhttp.js
+++ easyhttp.js
@@ -3,17 +3,59 @@ function EasyHttp() {
 }
 
 // Make an HTTP Get Request
-EasyHttp.prototype.get = function (url) {
+EasyHttp.prototype.get = function (url, callback) {
   this.http.open("GET", url, true);
   const that = this.http;
   this.http.onload = function () {
     if (that.status === 200) {
-      console.log(that.responseText);
+      // console.log(that.responseText);
+      callback(null, that.responseText);
+    } else {
+      callback(`Error: ${that.status}`);
     }
   };
   this.http.send();
 };
 
 // Make an HTTP POST Request
+EasyHttp.prototype.post = function (url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+  this.http.setRequestHeader("charset", "UTF-8");
+
+  const that = this;
+  this.http.onload = function () {
+    callback(null, that.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
 // Make an HTTP PUT Request
+EasyHttp.prototype.put = function (url, data, callback) {
+  this.http.open("PUT", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+  this.http.setRequestHeader("charset", "UTF-8");
+
+  const that = this;
+  this.http.onload = function () {
+    callback(null, that.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
 // Make an HTTP DELETE Request
+EasyHttp.prototype.delete = function (url, callback) {
+  this.http.open("DELETE", url, true);
+  const that = this.http;
+  this.http.onload = function () {
+    if (that.status === 200) {
+      // console.log(that.responseText);
+      callback(null, `Post Deleted`);
+    } else {
+      callback(`Error: ${that.status}`);
+    }
+  };
+  this.http.send();
+};
