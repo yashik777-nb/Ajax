0fde378d55284096a358c09c1dc060081c71bbc9
Yash <yaswanth.iddibani@gmail.com>
2020-06-14 01:27:20 +0100

    Fetch API

--- 06_fetchAPI.js
+++ 06_fetchAPI.js
@@ -5,28 +5,69 @@ document.getElementById("buttonFA3").addEventListener("click", getApiData);
 
 // Get Text
 function getText(e) {
-  fetch("data1.txt")
+  fetch("data.txt")
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       return response.text();
     })
     .then(function (data) {
-      console.log(data);
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(
+        data.replace("</title>", "</title></meta>"),
+        "application/xml"
+      );
+      if (!doc.querySelector("parsererror")) {
+        console.log(doc.querySelector("pre").innerHTML);
+        document.querySelector(".fetchApiData").innerHTML = doc.querySelector(
+          "pre"
+        ).innerHTML;
+      } else {
+        console.log(`Data: ${data}`);
+        document.querySelector(".fetchApiData").innerHTML = data;
+      }
+    })
+    .catch(function (err) {
+      console.log(`Error: ${err}`);
     });
-  // .catch(function (err) {
-  //   console.log(`Error: ${err}`);
-  // });
-  // e.preventDefault();
+  e.preventDefault();
 }
 
 // Get JSON
 function getJSON(e) {
-  console.log(e.target);
+  fetch("customers.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let output = "";
+      data.forEach(function (customer) {
+        output += `<li>${customer.name}, ${customer.company}</li>`;
+      });
+      document.querySelector(".fetchApiData").innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(`Error: ${err}`);
+    });
   e.preventDefault();
 }
 
 // Get API Data
 function getApiData(e) {
-  console.log(e.target);
+  fetch("https://api.github.com/users")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let output = "";
+      data.forEach(function (user) {
+        output += `<li>${user.login}</li>`;
+      });
+      document.querySelector(".fetchApiData").innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(`Error: ${err}`);
+    });
   e.preventDefault();
 }
