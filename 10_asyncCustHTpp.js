14c6bb70cf245183c464d084345451731bc602c8
Yash <yaswanth.iddibani@gmail.com>
2020-06-18 23:59:54 +0100

    Cust HTTP 3

---
+++ 10_asyncCustHTpp.js
@@ -0,0 +1,37 @@
+const http = new EasyHttp();
+
+// Get Users
+http
+  .get("https://jsonplaceholder.typicode.com/users1")
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+// Create Post
+const createData = {
+  title: "foo",
+  body: "bar",
+  userId: 1,
+};
+
+http
+  .post("https://jsonplaceholder.typicode.com/posts", createData)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+// Update Post
+const updateData = {
+  title: "foo",
+  body: "bar",
+  userId: 5,
+};
+
+http
+  .put("https://jsonplaceholder.typicode.com/posts/1", updateData)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+// Delete Post
+http
+  .delete("https://jsonplaceholder.typicode.com/posts/1", updateData)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
---
+++ easyHttp3.js
@@ -0,0 +1,46 @@
+class EasyHttp {
+  // Make a HTTP GET Request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make a HTTP POST Request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make an HTTP UPDATE Request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make a HTTP DELETE Request
+  async delete(url) {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const resData = await response.json();
+    return resData;
+  }
+}
